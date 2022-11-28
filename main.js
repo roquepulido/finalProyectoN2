9226cb8bf71caf913d7b2943ec6d95009dbcade1
Roque Pulido <roque.pulido@outlook.com>
2022-11-28 01:56:16 -0500

    Ajustes a template Button

--- main.js
+++ main.js
@@ -8,21 +8,19 @@ const countries = await fetch("./ExempleCountry.json").then(r=>r.json());
 
 
 gameCore.initGameCore(countries);
+DOMEdit.initTemplates();
 
 let quetion = gameCore.getQuestion();
 console.log(quetion);
 
 let nodo = document.getElementById("gameFrame");
 
-// if(quetion.typeQuestion == "capital") DOMEdit.printCapitalQ(quetion.quetions,nodo);
-// else DOMEdit.printFlagQ(quetion.quetions,nodo);
+ if(quetion.typeQuestion == "capital") DOMEdit.printCapitalQTemplate(quetion.quetions,nodo);
+ else DOMEdit.printFlagQTemplate(quetion.quetions,nodo);
 
-DOMEdit.initTemplates();
-
-console.log(DOMEdit.getTemplates());
-
-DOMEdit.printCapitalQTemplate(quetion.quetions,nodo);
-
+ document.getElementById("options").addEventListener("click",e =>{
+    console.log(e);
+ });
 
 
 
